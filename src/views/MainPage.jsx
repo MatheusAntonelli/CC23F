bd9bda96cd120241706ebca69eca1361a42206f7
Matheus Antonelli <matheusantonelli12@gmail.com>
2021-10-27 12:47:43 -0300

    Second commit update components from CC23F

---
+++ src/views/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainSection from '../Components/MainSection/MainSection'
+import Navbar from '../Components/Navbar/Navbar'
+
+function MainPage() {
+    return (
+        <>
+            <Navbar/>
+            <MainSection/>
+            <section id='atv-1'>
+                <p>Colocar aqui a primeira atividade.</p>
+            </section>
+        </>
+    )
+}
+
+export default MainPage
