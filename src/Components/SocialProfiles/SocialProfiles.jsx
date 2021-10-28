faaa7b057aab07039e9bc6922df922cb1d57aef1
Matheus Antonelli <matheusantonelli12@gmail.com>
2021-10-28 09:45:59 -0300

    noreferrer

--- src/Components/SocialProfiles/SocialProfiles.jsx
+++ src/Components/SocialProfiles/SocialProfiles.jsx
@@ -10,9 +10,9 @@ function SocialProfiles() {
                 <div className="iphone">
                     <img src="./img/smartphone.png" alt="smartphone" className='smart'/>
                     <div className='social-media'>
-                       <a href="https://www.instagram.com/matheusantonell1/" target="_blank"  className='link'><img src="./img/instagram.png" alt="insta"/ ></a> 
-                        <a href="https://github.com/MatheusAntonelli" target="_blank"  className='link'><img src="./img/github.png"  alt="Github"/></a>
-                        <a href="https://www.linkedin.com/in/matheus-antonelli-167004197/" target="_blank" className='link'><img src="./img/linkedin.png" alt="Linkedin"/></a>
+                       <a href="https://www.instagram.com/matheusantonell1/" target="_blank" rel="noreferrer"  className='link'><img src="./img/instagram.png" alt="insta"/ ></a> 
+                        <a href="https://github.com/MatheusAntonelli" target="_blank" rel="noreferrer" className='link'><img src="./img/github.png"  alt="Github"/></a>
+                        <a href="https://www.linkedin.com/in/matheus-antonelli-167004197/" rel="noreferrer" target="_blank" className='link'><img src="./img/linkedin.png" alt="Linkedin"/></a>
                     </div>
                 </div>
             </div>
