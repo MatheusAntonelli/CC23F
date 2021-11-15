f881c48f3fad18f58b759719ca06056f644ee154
matheus <matheusantonelli12@gmail.com>
2021-11-14 22:26:32 -0300

    Updating unused-vars

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -3,7 +3,6 @@ import Exercise1 from '../Components/Atv1/Exercise1'
 import MainSection from '../Components/MainSection/MainSection'
 import Navbar from '../Components/Navbar/Navbar'
 import SocialProfiles from '../Components/SocialProfiles/SocialProfiles'
-import Agrometeorologia from './Disciplinas/Agrometeorologia'
 
 function MainPage() {
     return (
